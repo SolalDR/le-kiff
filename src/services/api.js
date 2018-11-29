a47ff3d6a8ac63295f6c2b866865bba863e06603
SolalDR <solal.dussout-revel@hotmail.fr>
2018-11-29 11:55:26 +0100

    Add services

---
+++ src/services/api.js
@@ -0,0 +1,12 @@
+class API {
+    
+    static call(url){
+
+        // Promise.all waits until all jobs are resolved
+        // Promise.all(requests)
+        //   .then(responses => responses.forEach(
+        //     response => alert(`${response.url}: ${response.status}`)
+        //   ));
+    }
+
+}

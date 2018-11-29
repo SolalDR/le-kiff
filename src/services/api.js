0555c12e1bb8813aa9d0377ba244b881b764e0b0
SolalDR <solal.dussout-revel@hotmail.fr>
2018-11-29 13:58:50 +0100

    Update services

--- src/services/api.js
+++ src/services/api.js
@@ -4,21 +4,23 @@ class API {
     fetch(url)
     .then(res => res.json())
     .then(
+      
       (result) => {
-        this.setState({
-          isLoaded: true,
-          items: result.items
-        });
+
+        console.log("helloe", result);
+        // this.setState({
+        //   isLoaded: true,
+        //   items: result.items
+        // });
       },
-      // Note: it's important to handle errors here
-      // instead of a catch() block so that we don't swallow
-      // exceptions from actual bugs in components.
+
       (error) => {
-        this.setState({
-          isLoaded: true,
-          error
-        });
+        // this.setState({
+        //   isLoaded: true,
+        //   error
+        // });
       }
+        
     )
   }
 
---
+++ src/services/stores/store.js
@@ -0,0 +1,81 @@
+import { createStore } from 'redux'
+
+var myFuckingStore = createStore(()=>{
+  return {
+      // List of chapters store after api call
+      chapters: [
+        {
+          api_id: null || Object,
+          id: null,
+          slug: "chapter-1",
+          name: "Solal",
+          content: "Solal content"  
+        }
+      ],
+    
+      // List of all infos
+      infos: [
+        {
+          api_id: null || Object,
+          id: null,
+          name: "Solal",
+          type: "flux",
+          content: "Solal content"
+        }
+      ],
+    
+      // List of steps api call 
+      steps:  [
+        {
+          api_id: null || Object,
+          id: null,
+          name: "Solal",
+          content: "Solal content"
+        }
+      ]
+    }
+  }
+)
+
+var store = {
+
+  // List of chapters store after api call
+  chapters: [
+    {
+      api_id: null || Object,
+      id: null,
+      slug: "chapter-1",
+      name: "Solal",
+      content: "Solal content"  
+    }
+  ],
+
+  // List of all infos
+  infos: [
+    {
+      api_id: null || Object,
+      id: null,
+      name: "Solal",
+      type: "flux",
+      content: "Solal content"
+    }
+  ],
+
+  // List of steps api call 
+  steps:  [
+    {
+      api_id: null || Object,
+      id: null,
+      name: "Solal",
+      content: "Solal content"
+    }
+  ]
+
+
+
+  chapter: {
+    api_response: null || Object,
+  }
+
+}
+

9b65e13992ded9366051ceb791737f283abecc3f
Bastien CORNIER <bastien.cornier@gmail.com>
2018-12-03 16:58:42 +0100

    fix api import

--- src/services/AppManager.js
+++ src/services/AppManager.js
@@ -1,9 +1,11 @@
 import assetsManager from "./loaders/AssetsManager";
 import globalDatas from "./../datas/global.json";
-import Api from "../../services/Api";
+import Api from "./Api";
 
 class AppManager {
   constructor(){
+    this.api = new Api({ url:'https://le-kiff.bastiencornier.com/wp-json/v1' });
+
     assetsManager.loader.addGroup(globalDatas);
     assetsManager.loader.loadGroup("global");
   }

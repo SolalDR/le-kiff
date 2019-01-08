e76a3baf3e76056729907e625b7295471713a15a
Bastien CORNIER <bastien.cornier@gmail.com>
2019-01-08 20:41:22 +0100

    sound manager in step + sound options

--- src/webgl/components/SoundManager.js
+++ src/webgl/components/SoundManager.js
@@ -3,16 +3,44 @@ import {Howl, Howler} from 'howler'
 class SoundManager {
   constructor(){
     this.sounds = new Map();
+    this._volume = 0.5;
   }
 
-  addSound(name, path, args){
-    this.sounds.set(name, new Howl({
-      
-    }))
+  get volume() {
+    return this._volume;
+  }
+
+  set volume(v) {
+    Howler.volume(v);
+    this._volume = v
+  }
+
+  addSound(name, sound, options){
+    const soundObject = this.assignOptions(sound, options);
+    this.sounds.set(name, soundObject)
+  }
+
+  addSounds(soundArray) {
+    soundArray.forEach(element => {
+      this.addSound(element.name, element.sound, element.options)
+    });
   }
 
   play(sound) {
     this.sounds[sound].play();
+  }
+
+  playAll() {
+    this.sounds.forEach(function(sound, key) {
+      sound.play();
+    });
+  }
+
+  assignOptions(sound, options) {
+    Object.entries(options).forEach(([key, value]) => {
+      sound[key] = value;
+    })
+    return sound;
   }
 }
 

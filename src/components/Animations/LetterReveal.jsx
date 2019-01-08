8fe6037cf2342f25209626e62fc2f45f11732a93
Léa Tanda <leaztanda@gmail.com>
2019-01-08 14:04:54 +0100

    add letterreveal animation

--- src/components/Animations/LetterReveal.jsx
+++ src/components/Animations/LetterReveal.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from 'prop-types';
-import { TweenMax } from 'gsap';
+import { TimelineLite } from 'gsap';
 import "./styles.sass";
 
 class LetterReveal extends React.Component {
@@ -18,34 +18,49 @@ class LetterReveal extends React.Component {
     this.elements = React.createRef();
     this.letterEls = [];
     this.isRevealed = false;
+    this.isAnimating = false;
+    this.timeline = null;
+  }
+
+  componentDidMount() {
+    this.initReveal();
   }
 
   componentWillReceiveProps(nextProps) {
     if (nextProps.reveal !== this.props.reveal) {
-      this.isRevealed ?
-        this.unReveal() : this.reveal();
+      if (this.isRevealed && !nextProps.reveal) {
+        this.unReveal();
+      } else if (!this.isRevealed && nextProps.reveal) {
+        this.reveal();
+      }
     }
   }
 
+  initReveal() {
+    if (!this.timeline) {
+      this.timeline = new TimelineLite({paused: true, onReverseComplete: this.offAnimation.bind(this), onComplete: this.offAnimation.bind(this) });
+      this.timeline.staggerTo(this.letterEls,
+        this.props.duration,
+        { opacity: 1, top: 0 },
+        this.props.delay);
+    }
+  }
+
+  offAnimation() {
+    this.isAnimating = false;
+  }
+
   reveal() {
-    TweenMax.staggerFromTo(
-      this.letterEls,
-      this.props.duration,
-      { opacity: 0, top: 30 },
-      { opacity: 1, top: 0 },
-      this.props.delay
-    );
-    this.isRevealed = true;
+    if (!this.isAnimating) {
+      this.isAnimating = true;
+      this.timeline.play();
+      this.isRevealed = true;
+    }
   }
 
   unReveal() {
-    TweenMax.staggerFromTo(
-      this.letterEls,
-      this.props.duration,
-      { opacity: 1, top: 0 },
-      { opacity: 0, top: 30 },
-      this.props.delay
-    );
+    this.isAnimating = true;
+    this.timeline.reverse();
     this.isRevealed = false;
   }
 
--- src/components/Timeline/Timeline.jsx
+++ src/components/Timeline/Timeline.jsx
@@ -20,7 +20,7 @@ class Timeline extends React.Component {
     super(props);
     this.state = {
       current: 0,
-      reveals: [false, false]
+      reveal: null
     };
   }
 
@@ -33,28 +33,24 @@ class Timeline extends React.Component {
     this.props.onStepChangeCallBack(rank);
   };
 
-  onMouseOver(e, rank) {
-    if (!this.state.reveals[rank]) {
-      const currentReveals = this.state.reveals;
-      currentReveals[rank] = true;
-  
+  onMouseEnter(rank) {
+    if (this.state.reveal !== rank) {
       this.setState({
-        reveals: currentReveals, 
+        reveal: rank, 
       })
     }
   }
 
-  onMouseOut(e, rank) {
-    const currentReveals = this.state.reveals;
-    currentReveals[rank] = false;
-
-    this.setState({
-      reveals: currentReveals,
-    })
+  onMouseLeave(rank) {
+    if (this.state.reveal) {
+      this.setState({
+        reveal: null,
+      })
+    }
   }
 
   render() {
-    var results = [];
+    let results = [];
     for (let i = 1; i < (this.props.length + 1); i++) {
       (rank =>
         results.push(
@@ -66,23 +62,41 @@ class Timeline extends React.Component {
             active={this.props.current === rank}
           />
         ))(i);
+        
+    }
+
+    let previousChapter = null;
+    if (this.props.previousChapter && this.props.previousChapter.title) {
+      previousChapter = <h2 className="timeline__chapter timeline__chapter--prev heading-4" onMouseLeave={(e) => { this.onMouseLeave(1) }} onMouseEnter={(e) => { this.onMouseEnter(1) }}>
+                          <svg width="6" height="10" viewBox="0 0 6 10" className="timeline__chapter__control timeline__chapter__control--prev" xmlns="http://www.w3.org/2000/svg" >
+                            <path d="M1 1v8l5-4z" stroke="#FFFFFF" fill="none" fillRule="evenodd" />
+                          </svg>
+                          <LetterReveal text={`${this.props.previousChapter.rank}. ${this.props.previousChapter.title}`} duration={0.3} delay={0.01} reveal={(this.state.reveal && this.state.reveal === 1) ? true : false} />
+                        </h2>;
+    } else {
+      previousChapter = <span className="timeline__chapter--none"></span>;
+    }
+
+    let nextChapter = null;
+    if (this.props.nextChapter && this.props.nextChapter.title) {
+      nextChapter = <h2 className="timeline__chapter timeline__chapter--next heading-4" onMouseLeave={(e) => { this.onMouseLeave(2) }} onMouseEnter={(e) => { this.onMouseEnter(2) }}>
+                      <svg width="6" height="10" viewBox="0 0 6 10" className="timeline__chapter__control timeline__chapter__control--next" xmlns="http://www.w3.org/2000/svg" >
+                        <path d="M1 1v8l5-4z" stroke="#FFFFFF" fill="none" fillRule="evenodd" />
+                      </svg>
+                      <LetterReveal text={`${this.props.nextChapter.rank}. ${this.props.nextChapter.title}`} duration={0.3} delay={0.01} reveal={(this.state.reveal && this.state.reveal === 2) ? true : false} />
+                    </h2>;
+    } else {
+      nextChapter = <span className="timeline__chapter--none"></span>;
     }
 
     return <div className="timeline">
         {/* Conditional rendering for previous chapter */}
-        {this.props.previousChapter && this.props.previousChapter.title && <h2 className="timeline__chapter heading-4">
-            {this.props.previousChapter.rank}. {this.props.previousChapter.title}
-          </h2>}
+        {previousChapter}
 
         <div className="timeline__inner">{results}</div>
 
         {/* Conditional rendering for next chapter */}
-        {this.props.nextChapter && this.props.nextChapter.title && <h2 className="timeline__chapter heading-4">
-        <svg width="6" height="10" viewBox="0 0 6 10" xmlns="http://www.w3.org/2000/svg" onMouseOut={(e) => { this.onMouseOut(e, 1) }} onMouseOver={(e) => { this.onMouseOver(e, 1) }}>
-              <path d="M1 1v8l5-4z" stroke="#FFFFFF" fill="none" fillRule="evenodd" />
-            </svg>
-            <LetterReveal text={`${this.props.nextChapter.rank}. ${this.props.nextChapter.title}`} duration={0.3} delay={0.03} reveal={this.state.reveals[1]} />
-          </h2>}
+        {nextChapter}
       </div>;
   }
 }

1c7a5ed8c7d977b27b829452199f6a44ad34970e
Solal <solal.dussout-revel@hotmail.fr>
2019-01-09 13:09:47 +0100

    Initial automation

--- src/webgl/components/Scale/Micro/MicroScale.js
+++ src/webgl/components/Scale/Micro/MicroScale.js
@@ -3,7 +3,7 @@ import AssetsManager from "../../../../services/assetsManager/AssetsManager";
 import Scale from "../Scale";
 import * as THREE from "three";
 import { microConfig } from "~/webgl/config";
-import renderer from "~/webgl/rendering/Renderer"
+import Bus from "~/helpers/Bus";
 
 class MicroScale extends Scale {
 
@@ -14,6 +14,8 @@ class MicroScale extends Scale {
    */
   constructor(args){
     super({...args, name: "micro"});
+    this.molecules = new Map();
+
     this.state = {
       ...this.state
     }
@@ -43,30 +45,60 @@ class MicroScale extends Scale {
 
   // TODO Function updateFromStep
   updateFromStep( step ){
+    
+    var infos = step.infos.filter(info => info.scale === "micro" && info.type === "molecule");
 
+    this.molecules.forEach(molecule => {
+      var info = infos.find(info => info.slug = molecule.name);
+      if( info ){
+        molecule.attach(info);
+      } else {
+        molecule.detach();
+      }
+    })
+
+    infos.forEach(info => {
+      var molecule = this.molecules.get(info.slug);
+      if (!molecule) {
+        this.molecules.set(
+          info.slug
+        )  
+      }
+    })
+    
+    Bus.verbose("step:micro-update", 2)
+  }
+
+  initMoleculeMaterial(mat){
+    var env = new THREE.CubeTextureLoader().setPath( '/images/molecule/ldr/' ).load( ['px.png','nx.png','py.png','ny.png','pz.png','nz.png' ] );
+    mat.envMap = env;
+    mat.envMapIntensity = 2; 
+    mat.metalness = 1;
+    mat.roughness = 0.3;
+    mat.transparent = true;
+    mat.opacity = 0.85;
+    mat.needsUpdate = true;
+    return mat.material; 
   }
 
   /**
    * Init THREE.js part
    */
   initScene(e){
-    var env = new THREE.CubeTextureLoader().setPath( '/images/molecule/ldr/' ).load( ['px.png','nx.png','py.png','ny.png','pz.png','nz.png' ] );
-    e.molecule.result.scene.children[3].material.envMap = env; 
-    e.molecule.result.scene.children[3].material.envMapIntensity = 2; 
-    e.molecule.result.scene.children[3].material.metalness = 1;
-    e.molecule.result.scene.children[3].material.roughness = 0.3;
-    e.molecule.result.scene.children[3].material.transparent = true;
-    e.molecule.result.scene.children[3].material.opacity = 0.85;
+    var material = this.initMoleculeMaterial(e.molecule.result.scene.children[3])
 
-    e.molecule.result.scene.children[3].material.needsUpdate = true; 
+    var list = ["cocaine", "kerosene"];
 
-    this.molecules = {
-      cocaine: new Molecule({name: "cocaine", renderer: renderer, pdb: e.cocaine.result, material: e.molecule.result.scene.children[3].material}),
-      kerosene: new Molecule({name: "kerosene", renderer: renderer, pdb: e.kerosene.result, material: e.molecule.result.scene.children[3].material}),
-    }
-
-    this.group.add(this.molecules.cocaine.object3D);  
-    this.group.add(this.molecules.kerosene.object3D);  
+    list.forEach(item => {
+      this.molecules.set(item, new Molecule({
+        name: item,
+        pdb: e[item].result,
+        material
+      }));
+      this.group.add(this.molecules.get(item).object3D);
+    })
+    
+  
   }
 
   /**

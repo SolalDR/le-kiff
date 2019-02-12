3cbe334aa81d2415a7bf0eacad383bf1837f49be
Solal <solal.dussout-revel@hotmail.fr>
2019-02-12 12:35:53 +0100

    fix/radial-control-intensity

--- src/webgl/manager/Controller/config.js
+++ src/webgl/manager/Controller/config.js
@@ -2,7 +2,7 @@ import {c} from "~/helpers/Configuration"
 
 export default c({
   radial: {
-    amplitude: [0.3, 0.15],
+    amplitude: [0.2, 0.1],
     origin: new THREE.Vector3(),
     factor: 0.1
   }
